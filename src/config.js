ca41bd5cbb458e6c99552a7166b434e4c4579337
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-03-16 22:02:55 +0100

    Basic config loading

--- src/config.js
+++ src/config.js
@@ -1,4 +1,10 @@
-function config(defaults){
+const findConfig = require('find-config')
+const fs = require('fs')
+function config(baseDirectory,defaults){
+  const jsonConfig = findConfig('.yellow.json',{cwd:baseDirectory})
+  if (jsonConfig){
+    return Object.assign({},defaults,JSON.parse(fs.readFileSync(jsonConfig,'utf-8')))
+  }
   return defaults
 }
 
--- test/config.js
+++ test/config.js
@@ -1,10 +1,23 @@
-const expect = require('chai').expect;
+const expect = require('chai').expect
+var mock = require('mock-fs')
 const config = require('../src/config')
 
 describe('config', () => {
+  before(() => {
+    mock({
+      '/1/2/.yellow.json': '{"property":"yellow"}',
+      '/1/2/3/4': {}
+    })
+  })
+  after(() => mock.restore())
   it('Should return defaults', function () {
-    expect(config({
+    expect(config('/1/2/',{
       default: 'default'
     })).to.have.property('default', 'default')
+  })
+  it('Should load config', function () {
+    expect(config('/1/2/',{
+      default: 'default'
+    })).to.have.property('property', 'yellow')
   })
 })
